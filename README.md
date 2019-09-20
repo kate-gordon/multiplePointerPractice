cb44e1314d43b6e07773564bcb9fc57fa942e2aa
Kate Gordon <kategordon.code@gmail.com>
2019-09-20 15:27:46 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,33 @@
+
+THIS is a practice problem given at end of week4 at DigitalCrafts... 
+
+"Write a function called sumZero which accepts an array of integers. The 
+function should find the first pair where the sum is 0. Return an array that 
+includes both values that sum to zero or undefined if a pair does not exsist.
+ 
+
+[11,30,-1,4,3,9,-2,-1,0,-3,8,4,5,6,-6] = [-3, 3]
+
+ 
+
+ 
+
+Questions:
+
+ 
+
+What is the big(O) of your solution?
+
+ 
+
+ 
+
+What if the array WAS SORTED?
+
+
+
+[-6,-3,-2,-1,0,3,4,5,8,9] = [-3, 3]
+
+ 
+
+"
---
+++ scripts.js
@@ -0,0 +1,46 @@
+// sumZero UNSORTED 
+
+let unsortedArray = [11,30,-1,4,3,9,-2,-1,0,-3,8,4,5,6];
+let answer = [];
+function sumZero(arr) {
+  for(let i = 0; i <= unsortedArray.length -1; i++){
+    for(let j = 0; j <= unsortedArray.length-1; j++){
+        if (unsortedArray[i] + unsortedArray[j] !== 0){
+        continue;
+      }
+        if (unsortedArray[i] + unsortedArray[j] === 0){
+            answer = [unsortedArray[i], unsortedArray[j]];
+            return answer;
+        }
+    }  
+}
+return undefined; 
+}
+                          
+console.log(sumZero(unsortedArray));
+
+
+
+
+// Sorted Array Solution
+
+let array = [-6,-3,-2,-1,0,3,4,5,8,9]
+arr0 = []
+function sumZero(array) {
+    let i = 0;
+    let j = array.length - 1;
+    while (i < j) {
+    if (array[i] + array[j] > 0){
+        j--;
+    }
+    if (array[i] + array[j] < 0){
+        i ++;
+    }
+    if (array[i] + array[j] === 0 && i!=j) {
+        arr0 = [array[i], array[j]];
+        return arr0
+    }
+    }
+    return undefined
+}
+console.log(sumZero(array))
